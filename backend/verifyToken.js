28e09154a3a8c82ba12cc11195539e3730982807
yash-pouranik <yashpouranik124@gmail.com>
2026-02-15 01:05:23 +0530

    chore: Remove unused and temporary debug scripts

--- backend/verifyToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const verifyToken = (req,res,next)=>{
-    // BYPASS AUTHENTICATION
-    req.user = { id: "bypass_user", role: "admin" }; 
-    next();
-};
-
-module.exports = verifyToken;
